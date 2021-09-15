e31965e35015ac7763ae8d6044695ad6beb61739
Sam Klepper <samklepdev@gmail.com>
2021-09-15 07:59:39 -0500

    Initial commit

---
+++ components/Layout.js
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="keywords" content={keywords} />
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
+
+      <main>{children}</main>
+
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer">
+          Powered by{' '}
+          <span>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+Layout.defaultProps = {
+  title: 'WIG | Affordable Life Insurance',
+  keywords:
+    'WealthGuard, WIG, Insurance, Life Insurance, Insurance Quotes, Small Business Insurance',
+  description: 'WealthGuard Life Insurance, get an instant quote today!.',
+}
