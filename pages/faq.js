1242649b9f712ea6c835d6d95b9371a4ffdb2e99
Sam Klepper <samklepdev@gmail.com>
2021-09-22 15:02:35 -0500

    FAQ page content.

--- pages/faq.js
+++ pages/faq.js
@@ -1,9 +1,133 @@
 import Layout from '../components/Layout'
+import { ExternalLinkIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+
+const faqs = [
+  {
+    id: 1,
+    question: "What's the best thing about the US?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    id: 2,
+    question: "What's the best thing about the US?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    id: 3,
+    question: "What's the best thing about the US?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    id: 4,
+    question: "What's the best thing about the US?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    id: 5,
+    question: "What's the best thing about the US?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    id: 6,
+    question: "What's the best thing about the US?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+]
 
 export default function FaqPage() {
   return (
     <Layout>
-      <h1>FAQ Page</h1>
+      {/* Hero */}
+      <div className="bg-blue-200 lg:relative">
+        <div className="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
+          <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
+            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+              <span className="block xl:inline">FAQ</span>
+            </h1>
+            <p className="mt-3 max-w-md mx-auto text-lg text-gray-700 sm:text-xl md:mt-5 md:max-w-3xl">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+              fugiat aliqua.
+            </p>
+          </div>
+        </div>
+        <div className="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
+          <img
+            className="absolute inset-0 w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1484069560501-87d72b0c3669?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+            alt=""
+          />
+        </div>
+      </div>
+
+      {/* FAQs */}
+      <div className="bg-gray-50">
+        <div className="max-w-7xl mx-auto py-12 px-4 divide-y divide-gray-200 sm:px-6 lg:py-16 lg:px-8">
+          <h2 className="text-3xl font-extrabold text-gray-900">
+            Frequently asked questions
+          </h2>
+          <div className="mt-8">
+            <dl className="divide-y divide-gray-200">
+              {faqs.map((faq) => (
+                <div
+                  key={faq.id}
+                  className="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8">
+                  <dt className="text-base font-medium text-gray-900 md:col-span-5">
+                    {faq.question}
+                  </dt>
+                  <dd className="mt-2 md:mt-0 md:col-span-7">
+                    <p className="text-base text-gray-500">{faq.answer}</p>
+                  </dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+        </div>
+      </div>
+
+      {/* CTA */}
+      <div className="relative bg-gray-800">
+        <div className="h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
+          <img
+            className="w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1499159058454-75067059248a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+            alt=""
+          />
+        </div>
+        <div className="relative max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+          <div className="md:ml-auto md:w-1/2 md:pl-10">
+            <p className="mt-2 text-white text-3xl font-extrabold tracking-tight sm:text-4xl">
+              We’re here to help
+            </p>
+            <p className="mt-3 text-lg text-gray-300">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et,
+              egestas tempus tellus etiam sed. Quam a scelerisque amet
+              ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat
+              quisque ut interdum tincidunt duis.
+            </p>
+            <div className="mt-8">
+              <div className="inline-flex rounded-md shadow">
+                <Link href="/contact-us">
+                  <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50">
+                    Contact Us
+                    <ExternalLinkIcon
+                      className="-mr-1 ml-3 h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
