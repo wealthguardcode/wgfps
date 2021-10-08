d3f5d61ef59142760fd916394402e3a7eeef0cf9
Sam Klepper <samklepdev@gmail.com>
2021-10-08 17:00:38 -0500

    Form sending data to cms.

---
+++ components/PdfDownloadForm.js
@@ -0,0 +1,188 @@
+import { CloudDownloadIcon } from '@heroicons/react/outline'
+import emailjs from 'emailjs-com'
+
+function fun() {
+  document.getElementById('customer_name').value = ''
+  document.getElementById('customer_email').value = ''
+  document.getElementById('customer_phone').value = ''
+}
+
+function sendEmail(e) {
+  e.preventDefault()
+  if (
+    e.target.customer_name.value === '' ||
+    e.target.customer_email.value === '' ||
+    e.target.customer_phone.value === ''
+  ) {
+    return alert('Form cannot be empty!')
+  } else {
+    emailjs
+      .sendForm(
+        'service_74a7ngi',
+        'template_1gc83qd',
+        e.target,
+        'user_1ODnWoNdXKoQipSD1qXJf'
+      )
+      .then(
+        (result) => {
+          alert('Message sent!')
+          console.log(result.text)
+        },
+        (error) => {
+          alert(error.text)
+        }
+      )
+    fun()
+  }
+}
+
+export default function PdfDownloadForm() {
+  return (
+    <>
+      <div className="bg-white py-16 sm:py-24">
+        <div className="relative sm:py-16">
+          <div aria-hidden="true" className="hidden sm:block">
+            <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-50 rounded-r-3xl" />
+            <svg
+              className="absolute top-8 left-1/2 -ml-3"
+              width={404}
+              height={392}
+              fill="none"
+              viewBox="0 0 404 392">
+              <defs>
+                <pattern
+                  id="8228f071-bcee-4ec8-905a-2a059a2cc4fb"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse">
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={392}
+                fill="url(#8228f071-bcee-4ec8-905a-2a059a2cc4fb)"
+              />
+            </svg>
+          </div>
+          <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="relative rounded-2xl px-6 py-10 bg-blue-600 overflow-hidden shadow-xl sm:px-12 sm:py-20">
+              <div
+                aria-hidden="true"
+                className="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0">
+                <svg
+                  className="absolute inset-0 h-full w-full"
+                  preserveAspectRatio="xMidYMid slice"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 1463 360">
+                  <path
+                    className="text-blue-500 text-opacity-40"
+                    fill="currentColor"
+                    d="M-82.673 72l1761.849 472.086-134.327 501.315-1761.85-472.086z"
+                  />
+                  <path
+                    className="text-blue-700 text-opacity-40"
+                    fill="currentColor"
+                    d="M-217.088 544.086L1544.761 72l134.327 501.316-1761.849 472.086z"
+                  />
+                </svg>
+              </div>
+              <div className="relative">
+                <div className="sm:text-center">
+                  <h2 className="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">
+                    Get notified with our latest flyer.
+                  </h2>
+                  <p className="mt-6 mx-auto max-w-2xl text-lg text-blue-200">
+                    Enter you name, email and phone (optional) to start your
+                    download.
+                  </p>
+                </div>
+
+                <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                  <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                    <form
+                      className="space-y-6"
+                      method="POST"
+                      onSubmit={sendEmail}>
+                      {/* Name */}
+                      <div>
+                        <label
+                          htmlFor="name"
+                          className="block text-sm font-medium text-gray-700">
+                          Name
+                        </label>
+                        <div className="mt-1">
+                          <input
+                            id="customer_name"
+                            name="customer_name"
+                            type="name"
+                            autoComplete="name"
+                            required
+                            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <label
+                          htmlFor="email"
+                          className="block text-sm font-medium text-gray-700">
+                          Email
+                        </label>
+                        <div className="mt-1">
+                          <input
+                            id="customer_email"
+                            name="customer_email"
+                            type="email"
+                            autoComplete="email"
+                            required
+                            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                          />
+                        </div>
+                      </div>
+
+                      <div>
+                        <label
+                          htmlFor="phone"
+                          className="block text-sm font-medium text-gray-700">
+                          Phone
+                        </label>
+                        <div className="mt-1">
+                          <input
+                            id="customer_phone"
+                            name="customer_phone"
+                            type="phone"
+                            autoComplete="phone"
+                            required
+                            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                          />
+                        </div>
+                      </div>
+
+                      <div>
+                        <button
+                          type="submit"
+                          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                          Download <CloudDownloadIcon className="h-5 ml-2" />
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
