1fcbbb3da803632821e3350ab9c1dbfef6351f0c
Sam Klepper <samklepdev@gmail.com>
2021-10-12 17:56:08 -0500

    Content for hero on Kimberly's page.

--- pages/kim-mair.js
+++ pages/kim-mair.js
@@ -85,13 +85,14 @@ export default function KimMairPage() {
         <div className="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
           <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-              <span className="block xl:inline">Qui irure qui lorem</span>{' '}
-              <span className="block text-blue-600 xl:inline">Anim aute</span>
+              <span className="block lg:inline">
+                Because life’s <br />
+              </span>
+              <span className="block text-blue-600 xl:inline">too short.</span>
             </h1>
             <p className="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+              Ensuring that you are financially secure to face any type of
+              problem in life is what matters most.
             </p>
             <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
               <div className="rounded-md shadow">
