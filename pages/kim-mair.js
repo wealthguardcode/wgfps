80cec10d00bf992ae692e9378cacdc398b9492bb
Sam Klepper <samklepdev@gmail.com>
2021-10-12 18:03:49 -0500

    Kimberly page-- contact content.

--- pages/kim-mair.js
+++ pages/kim-mair.js
@@ -371,8 +371,8 @@ export default function KimMairPage() {
                   Contact information
                 </h3>
                 <p className="mt-6 text-base text-blue-50 max-w-3xl">
-                  Nullam risus blandit ac aliquam justo ipsum. Quam mauris
-                  volutpat massa dictumst amet. Sapien tortor lacus arcu.
+                  Have any question&apos;s or concerns? Do not hesitate to reach
+                  out to and someone will respond as soon as possible.
                 </p>
                 <dl className="mt-8 space-y-6">
                   <dt>
