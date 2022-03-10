bbf6751cd4c0986af7d3205964f9dceb3afedafd
Sam Klepper <samklepdev@gmail.com>
2022-03-10 11:46:23 -0600

    Header links font-size & spacing on sm/md screens preventing overflow.

--- components/HeaderNav.js
+++ components/HeaderNav.js
@@ -51,13 +51,13 @@ function classNames(...classes) {
 export default function Example() {
   return (
     <>
-      <Popover className="relative">
+      <Popover className='relative'>
         {({ open }) => (
           <>
             <Popover.Button
               className={classNames(
                 open ? 'text-gray-900' : 'text-gray-500',
-                'group bg-white rounded-md inline-flex items-center mt-5 text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                'group bg-white rounded-md inline-flex items-center mt-3 lg:mt-5 text-sm lg:text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
               )}>
               <span>Life Insurance</span>
               <ChevronDownIcon
@@ -65,27 +65,27 @@ export default function Example() {
                   open ? 'text-gray-600' : 'text-gray-400',
                   'ml-2 h-5 w-5 group-hover:text-gray-500'
                 )}
-                aria-hidden="true"
+                aria-hidden='true'
               />
             </Popover.Button>
 
             <Transition
               as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1">
-              <Popover.Panel className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0">
-                <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                  <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+              enter='transition ease-out duration-200'
+              enterFrom='opacity-0 translate-y-1'
+              enterTo='opacity-100 translate-y-0'
+              leave='transition ease-in duration-150'
+              leaveFrom='opacity-100 translate-y-0'
+              leaveTo='opacity-0 translate-y-1'>
+              <Popover.Panel className='absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0'>
+                <div className='rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden'>
+                  <div className='relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8'>
                     {policies.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
-                        className="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
-                        <p className="text-base font-medium text-gray-900">
+                        className='-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150'>
+                        <p className='text-base font-medium text-gray-900'>
                           {item.name}
                         </p>
                       </a>
@@ -97,13 +97,13 @@ export default function Example() {
           </>
         )}
       </Popover>
-      <Popover className="relative">
+      <Popover className='relative'>
         {({ open }) => (
           <>
             <Popover.Button
               className={classNames(
                 open ? 'text-gray-900' : 'text-gray-500',
-                'group bg-white rounded-md inline-flex items-center mt-5 text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                'group bg-white rounded-md inline-flex items-center mt-3 lg:mt-5 text-sm lg:text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
               )}>
               <span>Medicare</span>
               <ChevronDownIcon
@@ -111,27 +111,27 @@ export default function Example() {
                   open ? 'text-gray-600' : 'text-gray-400',
                   'ml-2 h-5 w-5 group-hover:text-gray-500'
                 )}
-                aria-hidden="true"
+                aria-hidden='true'
               />
             </Popover.Button>
 
             <Transition
               as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1">
-              <Popover.Panel className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0">
-                <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                  <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+              enter='transition ease-out duration-200'
+              enterFrom='opacity-0 translate-y-1'
+              enterTo='opacity-100 translate-y-0'
+              leave='transition ease-in duration-150'
+              leaveFrom='opacity-100 translate-y-0'
+              leaveTo='opacity-0 translate-y-1'>
+              <Popover.Panel className='absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0'>
+                <div className='rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden'>
+                  <div className='relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8'>
                     {medicare.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
-                        className="-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
-                        <p className="text-base font-medium text-gray-900">
+                        className='-m-3 p-3 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150'>
+                        <p className='text-base font-medium text-gray-900'>
                           {item.name}
                         </p>
                       </a>
@@ -143,15 +143,15 @@ export default function Example() {
           </>
         )}
       </Popover>
-      <Popover className="relative">
+      <Popover className='relative'>
         {({ open }) => (
           <>
             <Popover.Button
               className={classNames(
                 open ? 'text-gray-900' : 'text-gray-500',
-                'group bg-white rounded-md inline-flex items-center mt-5 text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                'group bg-white rounded-md inline-flex items-center mt-3 lg:mt-5 text-sm lg:text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
               )}>
-              <Link href="/annuities">
+              <Link href='/annuities'>
                 <a>
                   <span>Annuities</span>
                 </a>
@@ -160,15 +160,15 @@ export default function Example() {
           </>
         )}
       </Popover>
-      <Popover className="relative">
+      <Popover className='relative'>
         {({ open }) => (
           <>
             <Popover.Button
               className={classNames(
                 open ? 'text-gray-900' : 'text-gray-500',
-                'group bg-white rounded-md inline-flex items-center mt-5 text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                'group bg-white rounded-md inline-flex items-center mt-3 lg:mt-5 text-sm lg:text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
               )}>
-              <Link href="/employee">
+              <Link href='/employee'>
                 <a>
                   <span>Employee Benefits</span>
                 </a>
